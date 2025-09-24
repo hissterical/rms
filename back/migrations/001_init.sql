311406b16de04b4003322561196288903927d1cf
hissterical <paarivah@gmail.com>
2025-09-24 19:08:26 +0530

    modified migration sql file

--- back/migrations/001_init.sql
+++ back/migrations/001_init.sql
@@ -1,10 +1,13 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+CREATE TYPE room_status AS enum('available', 'reserved', 'occupied', 'maintenance');
+
 -- Properties (a hotel, motel, guesthouse)
 CREATE TABLE properties (
   id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
   name text NOT NULL,
-  created_at timestamptz DEFAULT now()
+  created_at timestamptz DEFAULT now(),
+  address varchar(255);
 );
 
 -- Rooms (belong to a property)
@@ -12,11 +15,22 @@ CREATE TABLE rooms (
   id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
   property_id uuid REFERENCES properties(id) ON DELETE CASCADE,
   room_number text NOT NULL,
-  room_type text,
-  capacity int DEFAULT 1,
-  created_at timestamptz DEFAULT now(),
-  UNIQUE(property_id, room_number)
+  UNIQUE(property_id, room_number),
+  room_type_id uuid REFERENCES room_types(id) ON DELETE SET NULL,
+  floor int, 
+  status room_status default 'available',
+  booking_id uuid REFERENCES bookings(id) ON DELETE CASCADE,
+
 );
+
+CREATE TABLE room_types (
+  id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+  room_type_name text NOT NULL,
+  description text,
+  capacity int DEFAULT 1,
+  price int,
+  property_id uuid REFERENCES properties(id) ON DELETE CASCADE,
+)
 
 -- Customers (guests)
 CREATE TABLE customers (
