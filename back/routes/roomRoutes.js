88c2e39e5efad919b262ba56e4248af54d520c35
shash <1ms23is113@msrit.edu>
2025-11-16 22:16:15 +0530

    Add files via upload

--- back/routes/roomRoutes.js
+++ back/routes/roomRoutes.js
@@ -5,6 +5,9 @@ import {
   createRoom,
   deleteRoom,
   updateRoom,
+  updateRoomStatus,
+  getAvailableRooms,
+  bulkCreateRooms,
 } from "../controllers/roomController.js";
 
 const router = express.Router();
@@ -13,14 +16,28 @@ const router = express.Router();
 // Hotel owners should not use these routes.
 // Use propertyRoutes.js for creating/updating rooms under a property.
 
+// Get all rooms for a property
 router.get("/", getAllRooms);
 
+// Get available rooms for date range
+router.get("/available", getAvailableRooms);
+
+// Get single room by ID
 router.get("/:id", getRoomById);
 
+// Create new room
 router.post("/", createRoom);
 
-router.delete("/:id", deleteRoom);
+// Bulk create rooms
+router.post("/bulk", bulkCreateRooms);
 
+// Update room status only
+router.patch("/:id/status", updateRoomStatus);
+
+// Update entire room
 router.put("/:id", updateRoom);
+
+// Delete room
+router.delete("/:id", deleteRoom);
 
 export default router;
